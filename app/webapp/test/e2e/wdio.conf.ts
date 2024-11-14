1cd8bea63e26e7a353f9a26873a7e0e93077bc01
Mads <mrj@gavdilabs.com>
2024-11-14 13:45:01 +0100

    Added necessary geckodriver

--- app/webapp/test/e2e/wdio.conf.ts
+++ app/webapp/test/e2e/wdio.conf.ts
@@ -155,7 +155,7 @@ export const config: wdi5Config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ["ui5"],
+    services: ["geckodriver","ui5"],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
